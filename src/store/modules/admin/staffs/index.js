2526b715ca47b9412b79b62642aaf08ae7caa3e2
huybq-zigexnvn <huybq@zigexn.vn>
2023-01-02 00:10:54 +0700

    update

--- src/store/modules/admin/staffs/index.js
+++ src/store/modules/admin/staffs/index.js
@@ -1,23 +1,49 @@
 import axios from "axios";
+import { useRouter } from 'vue-router'
+
 
 const STAFF_LIST = 'STAFF_LIST';
+// const STAFF = 'STAFF';
+const router = useRouter();
 
 const state = {
-  staff_list: []
+  staff_list: [],
+  staff: {
+    email: '',
+    name: '',
+    password: ''
+  }
 };
 
 export const mutations = {
   [STAFF_LIST] (state, data) {
     state.staff_list = data;
   },
-
+  // [STAFF] (state, data) {
+  //   state.staff.email = data;
+  // }
 };
 
 export const actions = {
-  getList({ commit }) {
+  async getList({ commit }) {
     axios.get('http://localhost:3000/api/v1/staffs').then((response) => {
       let staffList = response.data
       commit(STAFF_LIST, staffList);
+    }).catch(error => {
+      console.error(error)
+    })
+  },
+  createStaff({ state }) {
+    axios.post('http://localhost:3000/api/v1/create_staff', {
+      email: state.staff.email,
+      name: state.staff.name,
+      password: state.staff.password,
+    }).then((response) => {
+      let staff = response.data
+      // commit(STAFF, staff);
+      if(staff.success) {
+        router.push({name: 'Home'})
+      }
     }).catch(error => {
       console.error(error)
     })
@@ -26,7 +52,7 @@ export const actions = {
 
 export const getters = {
   staffList: state => state.staff_list,
-  
+  staff: state => state.staff,
 };
 
 export default {
--- src/views/admin/ListStaffs.vue
+++ src/views/admin/ListStaffs.vue
@@ -3,7 +3,7 @@
     <div class="d-flex justify-content-end align-items-center activity">
       <div><a class="btn btn-primary" href="#" @click="() => { visibleModal = true }">Create Staff</a></div>
     </div>
-    <div v-if="staffList.length > 0">
+    <!-- <div v-if="staffList.length > 0">
       <div class="col-md-12">
         <div class="mt-3">
           <ul class="list list-inline">
@@ -21,32 +21,52 @@
         </div>
       </div>
     </div>
-    <div v-else><span>Don't have any staff yet</span></div>
+    <div v-else><span>Don't have any staff yet</span></div> -->
+  </div>
+  <div class="responsive-table">
+    <div class="table-header row">
+      <div class="col col-2"></div>
+      <div class="col col-5">User Name</div>
+      <div class="col col-5">Email</div>
+    </div>
+    <div class="table-row row" data-url="" v-for="staff in staffList" :key="staff">
+      <div class="col col-2 text-center">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+          <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+          <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+        </svg>
+      </div>
+      <div class="col col-5">{{staff['name']}}</div>
+      <div class="col col-5">{{staff['email']}}</div>
+    </div>
+    <div class="p-8"></div>
   </div>
   <CModal alignment="center" :visible="visibleModal" @close="() => { visibleModal = false }">
     <CModalHeader>
       <CModalTitle>Form create staff</CModalTitle>
     </CModalHeader>
-    <CModalBody>
-      <div class="mb-3">
-        <label for="staffEmail" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="staffEmail" placeholder="name@example.com">
-      </div>
-      <div class="mb-3">
-        <label for="staffName" class="form-label">Staff name</label>
-        <input type="name" class="form-control" id="staffName">
-      </div>
-      <div class="mb-3">
-        <label for="staffPassword" class="form-label">Passwords</label>
-        <input type="password" class="form-control" id="staffPassword">
-      </div>
-    </CModalBody>
-    <CModalFooter>
-      <CButton color="secondary" @click="() => { visibleModal = false }">
-        Close
-      </CButton>
-      <CButton color="primary">Save</CButton>
-    </CModalFooter>
+    <form @submit="createStaff()">
+      <CModalBody>
+        <div class="mb-3">
+          <label for="staffEmail" class="form-label">Email address</label>
+          <input type="email" class="form-control" id="staffEmail" placeholder="name@example.com" v-model="staff.email">
+        </div>
+        <div class="mb-3">
+          <label for="staffName" class="form-label">Staff name</label>
+          <input type="name" class="form-control" id="staffName" v-model="staff.name">
+        </div>
+        <div class="mb-3">
+          <label for="staffPassword" class="form-label">Passwords</label>
+          <input type="password" class="form-control" id="staffPassword" v-model="staff.password">
+        </div>
+      </CModalBody>
+      <CModalFooter>
+        <CButton color="secondary" @click="() => { visibleModal = false }">
+          Close
+        </CButton>
+        <CButton type="submit" color="primary">Save</CButton>
+      </CModalFooter>
+    </form>
   </CModal>
 </template>
 
@@ -62,17 +82,23 @@ export default {
   },
   computed: {
     ...mapGetters({
-      staffList: 'adminStaffs/staffList'
+      staffList: 'adminStaffs/staffList',
+      staff: 'adminStaffs/staff',
     }),
   },
 
   methods: {
     ...mapActions({
-      getStaffList: 'adminStaffs/getList'
+      getStaffList: 'adminStaffs/getList',
+      createStaff: 'adminStaffs/createStaff',
     }),
+    staff_info(){
+      // console.log(staff);
+    }
   },
   mounted() {
     this.getStaffList();
+    // this.createStaff();
   }
 }
 </script>
