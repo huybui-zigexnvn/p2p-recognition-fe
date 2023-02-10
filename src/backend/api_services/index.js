6cee65c1e561d57de4651d902f13946b9a7d0720
huybq-zigexnvn <huybq@zigexn.vn>
2023-02-10 13:46:29 +0700

    update

--- src/backend/api_services/index.js
+++ src/backend/api_services/index.js
@@ -1,30 +1,30 @@
 import axios from 'axios';
 
 const ApiService = {
-	init(baseURL) {
-		axios.defaults.baseURL = baseURL;
-		axios.interceptors.request.use(
-			(config) => {
-				return config;
-			}
-		);
-	},
+  init(baseURL) {
+    axios.defaults.baseURL = baseURL;
+    axios.interceptors.request.use(
+      (config) => {
+        return config;
+      }
+    );
+  },
 
-	get(resource, params={}) {
-		return axios.get(resource, { params });
-	},
+  get(resource, params={}) {
+    return axios.get(resource, { params });
+  },
 
-	post(resource, data, options={}) {
-		return axios.post(resource, data, options);
-	},
+  post(resource, data, options={}) {
+    return axios.post(resource, data, options);
+  },
 
-	put(resource, data) {
-		return axios.put(resource, data);
-	},
+  put(resource, data) {
+    return axios.put(resource, data);
+  },
 
-	delete(resource, data = {}) {
-		return axios.delete(resource, { data: data });
-	},
+  delete(resource, data = {}) {
+    return axios.delete(resource, { data: data });
+  },
 };
 
 export default ApiService;
