78e1a14b7eb7190d7926a9904018f07664271d19
hieppv-zigexn <112839753+hieppvZigexn@users.noreply.github.com>
2023-05-17 21:19:30 +0700

    Fix login (#32)
    * Fix login
    
    * Fix birth day

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -17,6 +17,8 @@ import { CContainer } from '@coreui/vue'
 import AppHeader from '@/components/AppHeader.vue'
 import AppSidebar from '@/components/AppSidebar.vue'
 import Loader from '@/components/Loader'
+import AuthApi from "@/backend/auth";
+import defineAbilityFor from '@/services/ability'
 
 export default {
   name: 'DefaultLayout',
@@ -25,6 +27,15 @@ export default {
     AppSidebar,
     CContainer,
     Loader
-  }
+  },
+  async created() {
+      let role = ''
+      await AuthApi.getCurrentUser().then(response => {
+              role = response.data.role
+            }).catch(error => {
+              console.log(error)
+            })
+      defineAbilityFor(role)
+    }
 }
 </script>
